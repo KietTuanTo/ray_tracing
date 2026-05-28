9f647070bc9b31cf51af75dc8ecf1ca83e85f4de
Alex To <alexto188888@gmail.com>
2026-05-29 02:53:02 +1000

    refactoring to remove inline

--- vec3/color.h
+++ vec3/color.h
@@ -5,14 +5,4 @@
 
 using color = vec3;
 
-inline void write_colour(std::ostream& out, const color& pixel_colour) {
-    auto r = pixel_colour.x();
-    auto g = pixel_colour.y();
-    auto b = pixel_colour.z();
-
-    int rbyte = int(255.999 * r);
-    int gbyte = int(255.999 * g);
-    int bbyte = int(255.999 * b);
-
-    out << rbyte << " " << gbyte << " " << bbyte << '\n';
-}
+void write_colour(std::ostream& out, const color& pixel_colour);
