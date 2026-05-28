1874021a186a35d729cfb7ef13278853f3112803
Alex To <alexto188888@gmail.com>
2026-05-28 17:48:42 +1000

    created colour alias and updated ppm_renderer

--- ppm_renderer/ppm_renderer.cpp
+++ ppm_renderer/ppm_renderer.cpp
@@ -1,3 +1,5 @@
+#include "vec3/vec3.h"
+#include "vec3/color.h"
 #include <iostream>
 
 int main() {
@@ -12,14 +14,8 @@ int main() {
     for (int i = 0; i < image_width; ++i) {
         std::clog << "\rScanlines remaining: " << (image_height - i) << " " << std::flush;
         for (int j = 0; j < image_height; ++j) {
-            auto r = (double) i / (image_width - 1);
-            auto g = (double) j / (image_height - 1);
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = 0;
-
-            std::cout << ir << " " << ig << " " << ib << '\n';
+            auto pixel_colour = color((double) i / (image_width - 1), (double) j / (image_height - 1), 0);
+            write_colour(std::cout, pixel_colour);
         }
     }
 
---
+++ vec3/color.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "vec3.h"
+#include <iostream>
+
+using color = vec3;
+
+void write_colour(std::ostream& out, const color& pixel_colour) {
+    auto r = pixel_colour.x();
+    auto g = pixel_colour.y();
+    auto b = pixel_colour.z();
+
+    int rbyte = int(255.999 * r);
+    int gbyte = int(255.999 * g);
+    int bbyte = int(255.999 * b);
+
+    out << rbyte << " " << gbyte << " " << bbyte << '\n';
+}
